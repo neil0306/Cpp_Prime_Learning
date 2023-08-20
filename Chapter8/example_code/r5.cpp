17736119ae413e0aefd082141d4dd636c7ec3a25
neil <719860478@qq.com>
2023-08-20 14:58:32 +0800

    完成第八章复习题

---
+++ Chapter8/example_code/r5.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <array>
+#include <string>
+
+using namespace std;
+const int SEASONS = 4;
+
+void fill(array<double, SEASONS> &pa);
+void show(const array<double, SEASONS> &da);
+
+const array<string, SEASONS> Snames = {"Spring", "Summer", "Fall", "Winter"};  // 初始化四个季节的名字
+
+int main(void)
+{
+    array<double, SEASONS> expenses;  // expenses 用来存放四个季节的开销
+    fill(expenses);                  // 防止拷贝, 传指针
+    show(expenses);
+
+    return 0;
+}
+
+void fill(array<double, SEASONS> &pa)           // 要修改数组, 所以不加 const 做保护
+{
+    for(int i = 0; i < SEASONS; i++){
+        cout << "Enter " << Snames[i] << " expenses: ";
+        cin >> pa[i];                        // (*pa) 先将指针转换为 array 对象, 只有 array 对象可以像数组一样操作, 但是array指针是不可以直接像指针一样的操作的, 因为array指针"并不表示第数组的第一个元素的地址"!
+    }
+}
+
+void show(const array<double, SEASONS> &da)
+{
+    double total = 0.0;
+    cout << "EXPENSES: " << endl;
+    for (int i = 0; i < SEASONS; i++){
+        cout << Snames[i] << "\t: $" << da[i] << endl;
+        total += da[i];
+    }
+    cout << "Total: " << total << endl;
+}
