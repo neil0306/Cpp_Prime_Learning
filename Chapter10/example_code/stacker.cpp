f76278c62f39d9d24e118248c5ea11bc229644b0
neil <719860478@qq.com>
2023-10-03 17:52:32 +0800

    第十章: 通用数据类型(ADT), 类的作用域

---
+++ Chapter10/example_code/stacker.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cctype>       // 为了使用 toupper()
+#include "stack.h"
+
+using namespace std;
+
+int main(void)
+{
+    char ch;
+    int po;       // 定义用来放入栈里的数据, 如果这个数据类型改变了, 则stack.h 里的 ITEM 对应的类型也要改变
+    Stack st;               // 实例化一个 "栈" 对象
+
+    cout << "Please Enter A to push to stack, \n"
+        << "P to pop from stack, \n"
+        << "Q to quit." << endl;
+
+    // 判断输入是否合法, 并且判断是否对栈进行操作
+    while(cin >> ch && toupper(ch) != 'Q'){
+        while(cin.get() != '\n'){        // 处理缓冲区里的回车
+            continue;
+        }
+        switch(ch)        
+        {
+            case 'A':
+            case 'a':
+                cout << "Enter a number you want to push to stack...\n";
+                cin >> po;
+                if(st.isFull()){
+                    cout << "stack already full!!" << endl;
+                }
+                else{
+                    st.push(po);
+                    cout << "Push " << po << " to stack!" << endl;
+
+                }
+                break;
+            case 'P':
+            case 'p':
+                if(st.isEmpty()){
+                    cout << "Stack is Empty!" << endl;
+                }
+                else{
+                    st.pop(po);
+                    cout << po << " is poped!" << endl;
+                }
+                break;
+        }
+
+        // 提示输入下一组数据
+        cout << "Please Enter A to push to stack, \n"
+        << "P to pop from stack, \n"
+        << "Q to quit." << endl;
+    }
+    return 0;
+}
